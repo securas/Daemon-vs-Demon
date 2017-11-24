1ae70201a93d610d4615e626b781dccbbc8b3487
securas <ruben.so.luis@gmail.com>
2017-11-24 20:45:36 +0900

    modified rain script... slightly faster

--- gameoffgame/scripts/rain.gd
+++ gameoffgame/scripts/rain.gd
@@ -11,25 +11,25 @@ var drops = []
 class Drop:
 	var instance = null
 	var frame = 0
-	var timer = 0
+	#var timer = 0
 	var has_parent = false
 	var update_rate = 0.05
+	var frame_speed = 1
+	var float_frame = 0
 	var final_frame = 27
 	var active = true
-	func update( delta ):
-		timer -= delta
-		var output = false
-		if timer <= 0:
-			if frame < 23:
-				timer = update_rate
-			else:
-				timer = 0.15
-			frame = ( frame + 1 ) % final_frame
-			if frame != instance.get_frame():
-				instance.set_frame( frame )
-				if frame == 0:
-					return true
-		return false
+	func update():
+		#var out = false
+		float_frame += 0.1
+		frame = int( float_frame / update_rate )
+		if frame >= final_frame:
+			frame = 0
+			float_frame = 0
+			#out = true
+		#frame = ( frame + 1 ) % final_frame
+		instance.set_frame( frame )
+		return ( frame == 0 )
+
 
 var _stop_rain = false
 func stop():
@@ -53,22 +53,38 @@ func _ready():
 	set_fixed_process( true )
 
 var gpos = Vector2()
+var timer = 0.0
 func _fixed_process( delta ):
-	#print( gpos, " ", drops[0].instance.get_global_pos(), " ", drops[0].frame )
-	gpos = get_global_pos()
-	for d in drops:
-		if not d.active: continue
-		if not d.has_parent:
-			parent_node.add_child( d.instance )
-			d.has_parent = true
-			_set_random_pos( d )
-		if not _stop_rain:
-			if d.update( delta ):
+	timer -= delta
+	if timer <= 0:
+		timer = 0.1
+		gpos = get_global_pos()
+		for d in drops:
+			if not d.has_parent:
+				parent_node.add_child( d.instance )
+				d.has_parent = true
 				_set_random_pos( d )
-		else:
-			if d.update( delta ):
-				d.active = false
-				d.instance.queue_free()
+			if not _stop_rain:
+				if d.update(): _set_random_pos( d )
+			else:
+				if d.update():
+					d.active = false
+					d.instance.queue_free()
+	
+	
+#	for d in drops:
+#		if not d.active: continue
+#		if not d.has_parent:
+#			parent_node.add_child( d.instance )
+#			d.has_parent = true
+#			_set_random_pos( d )
+#		if not _stop_rain:
+#			if d.update( delta ):
+#				_set_random_pos( d )
+#		else:
+#			if d.update( delta ):
+#				d.active = false
+#				d.instance.queue_free()
 	
 	# follow target
 	if game.player != null and game.player.get_ref() != null:
@@ -89,8 +105,6 @@ func _set_random_pos( d ):
 
 	
 
-		
-	#var results = space_state.intersect_ray( get_global_pos(), game.player.get_ref().get_global_pos(), [ self ] )
 func _check_ground( gpos ):
 	var tilepos = ground_node.world_to_map( gpos )
 	if ground_node.get_cell( tilepos.x, tilepos.y ) == -1:
